f41d0c67a573dc0154992bcfdbb02c69f54606a3
helo <111387874+helo-stoco@users.noreply.github.com>
2024-07-24 09:32:00 -0300

    style

--- script.js
+++ script.js
@@ -42,3 +42,15 @@ const perguntas = [
         ]
     }
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    }
+    
+    mostraPergunta();
+
+
