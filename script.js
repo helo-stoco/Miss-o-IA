2047608387647c7628d7599f5b3b9396e7e6a181
helo <111387874+helo-stoco@users.noreply.github.com>
2024-07-25 14:40:12 -0300

    kkkkkk

--- script.js
+++ script.js
@@ -49,7 +49,15 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    }
+    mostraAlternativas();
+}
+function mostraPergunta() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+    botaoAlternativa.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+}
+}
     
     mostraPergunta();
 
