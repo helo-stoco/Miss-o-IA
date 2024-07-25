1a1da497d0153ed3beabc294ffd2777f9a8f0a81
helo <111387874+helo-stoco@users.noreply.github.com>
2024-07-25 19:05:56 -0300

    salvar

--- script.js
+++ script.js
@@ -74,24 +74,27 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
-function mostraPergunta() {
+function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-    botaoAlternativa.textContent = alternativa.texto;
-    botaoAlternativas.addEventListener("click", function(){
-        atual++;
-        mostraPergunta();
-    })
-    caixaAlternativas.appendChild(botaoAlternativas);
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
-}
-    
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
     mostraPergunta();
+}
+mostraPergunta();
 
 
